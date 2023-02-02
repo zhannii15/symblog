caccbe183baddfb84b2b6c34af3f062db7c9636c
Awa DIALLO <zhanniidiallo1@gmail.com>
2023-02-02 17:10:34 +0100

    modif Articles+Categorie,modif vues Categorie

--- src/DataFixtures/Blogfixture.php
+++ src/DataFixtures/Blogfixture.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Faker\Factory;
 use DatetimeImmutable;
 use App\Entity\Articles;
+use App\Entity\Categorie;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -15,18 +16,23 @@ class Blogfixture extends Fixture
         //pour utiliser FAKER
 
         $faker =\Faker\Factory::create('fr_FR');
-
-        //Fabriquer plusieurs articles
-        for($i=0;$i<10;$i++){
-            // Génération d'un article
-            $art = new Articles();
-            $art->setTitle($faker->sentence());
-            $art->setContent($faker->paragraph(5,true));
-            $art->setCreatedAt(new DatetimeImmutable());
-            $art->setPicture("https://loremflickr.com/320/240/loup");
-
-            // persisté cet article
-            $manager->persist($art);
+        
+        for($c=0;$c<4;$c++){
+            $cat = new Categorie();
+            $cat->setName($faker->word());
+            $manager->persist($cat);
+            // entre 1 et 4 par catégorie
+            for($i=0;$i<mt_rand(1,4);$i++){
+                // Génération d'un article
+                $art = new Articles();
+                $art->setTitle($faker->sentence());
+                $art->setContent($faker->paragraph(5,true));
+                $art->setCreatedAt(new DatetimeImmutable());
+                $art->setPicture("https://loremflickr.com/320/240/loup");
+                $art->setCategory($cat);
+                // persisté cet article
+                $manager->persist($art);
+            }
         }
         // Ecriture dans la BDD
         $manager->flush();
